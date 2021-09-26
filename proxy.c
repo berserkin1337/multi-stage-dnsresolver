003295ed6affd0a419c671315b351765108ded6e
berserkin1337 <gaviral373@gmail.com>
2021-09-26 12:46:24 +0530

    test

--- proxy.c
+++ proxy.c
@@ -2,4 +2,11 @@
 
 #define MAXLINE 4096
 #define SA struct sockaddr
-int main(int argc, char *argv[]) { return 0; }
+int main(int argc, char *argv[]) {
+
+  int listenfd, connfd, n;
+  struct sockaddr_in servaddr;
+  uint8_t buff[MAXLINE + 1];
+  uint8_t recvline[MAXLINE + 1];
+  return 0;
+}
